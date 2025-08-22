d94045cdc77752308554957a680e5d416ded81ae
ekkapot.c@tggs.kmutnb.ac.th <ekkapot.c@tggs.kmutnb.ac.th>
2025-08-23 00:40:17 +0700

    add sine

---
+++ sine/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(int argc,char * argv[]){
+  	double x = 1.44;
+	double y;
+
+	__asm__ __volatile__ (
+	  "fldl %[_x]\n\t"
+	  "fsin \n\t"
+	  "fstpl %[_y]\n\t"
+	  :[_y]"=m"(y)
+ 	  :[_x]"m"(x)
+	  :"st"
+	);
+
+	std::cout << "y = " << y << std::endl;
+	return 0;
+}
